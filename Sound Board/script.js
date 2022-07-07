21c074f439ccb1db0192af2f4bcba48adfe5da9d
Baglan <kxelly@mail.ru>
2022-07-07 11:23:11 +0200

    Sound Board

---
+++ Sound Board/script.js
@@ -0,0 +1,26 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        var song = document.getElementById(sound)
+        song.volume = 0.1;
+        song.play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
